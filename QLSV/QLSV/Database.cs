69db18035ad65251a1a9c24598cfe519ee0e21fb
kennguen2503 <52605206+kennguen2503@users.noreply.github.com>
2021-08-19 15:57:03 +0700

    tạo pj
    kết nối csdl

---
+++ QLSV/QLSV/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV
+{
+    public class Database
+    {
+        private string connetionString = @"Data Source=localhost;Initial Catalog = QLSV;User ID = sa;Password = 123456789";
+        private SqlConnection conn;
+        private string sql;
+        private DataTable dt;
+        private SqlCommand cmd;
+
+        public Database()
+        {
+            try
+            {
+                conn = new SqlConnection(connetionString);
+                //conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("connected failed" + ex.Message);
+            }
+
+        }
+        public DataTable SelectData()
+        {
+            try
+            {
+                conn.Open();
+                sql = "select * from SinhVien";
+                cmd = new SqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Lỗi Load dữ liệu: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
